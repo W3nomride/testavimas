52961c37b3a39072b6dd03bfe8dd43085bbfa74b
saulius <wenomride@gmail.com>
2019-04-17 17:02:30 +0300

    veikti turi

---
+++ Desktop/testukas/public_html/index.php
@@ -0,0 +1,31 @@
+<?php
+$sudas1 = rand(1, 100);
+$sudas2 = rand(101, 200);
+$sudas3 = rand(201, 300);
+$sudas4 = rand(301, 400);
+?>
+<html>
+
+    <head        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <title>uzduotis</title>
+    </head>
+    <body>
+        <ul>
+            <div>
+                 <h1
+                     Skolos skaiciuokle
+            </h1>
+                 <h2>
+                     Jei paemei <?php print $sudas1;?> jevriuku
+                </h2>
+                 <h3>
+                     Su dviem kabanciais grazinsi<?php print $sudas3?>
+                </h3>
+                 <h4>
+                     Su vienu kabanciu grazinsi<?php print $sudas2;?>
+                </h4>
+        </div>
+                
+    </body>
+</html>
+
